b3fa83eb8c110f467d183917786a582e18ab6f1f
acheng <964067013@qq.com>
2018-06-26 17:55:17 +0800

    1

---
+++ 假坦克打桩/Assets/EnemyMove.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMove : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        int value = 3;
+        int flag = 1;
+        while (true)
+        {
+            float sum = 0;
+            if (flag == 1)
+            {
+                this.transform.Translate(flag * 2 * Time.deltaTime, 0, 0);
+                sum += flag * 2 * Time.deltaTime;
+                if (sum == value)
+                    flag = -1;
+            }
+
+            if (flag == -1)
+            {
+                this.transform.Translate(flag * 2 * Time.deltaTime, 0, 0);
+                sum -= flag * 2 * Time.deltaTime;
+                if (sum == value)
+                    flag = 1;
+            }
+        }
+	}
+}
